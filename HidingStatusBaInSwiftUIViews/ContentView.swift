a3b4dad72a8956bcd14792a3527787cecb86181d
ribragimov-mwb <ribragimov@malwarebytes.com>
2024-06-08 15:06:12 +0300

    with NavigationStack

--- HidingStatusBaInSwiftUIViews/ContentView.swift
+++ HidingStatusBaInSwiftUIViews/ContentView.swift
@@ -9,18 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var hideStatus = false
+    @State private var showModel = false
     
     var body: some View {
-        VStack {
-            LabeledContent("SwiftUI") {
-                Text("Learn iOS")
-                    .foregroundStyle(.mint)
+        NavigationStack {
+            NavigationLink {
+                Text("This is the destination")
+            } label: {
+                VStack {
+                    LabeledContent("SwiftUI") {
+                        Text("Learn iOS")
+                            .foregroundStyle(.mint)
+                    }
+                    .font(.largeTitle)
+                    
+                    Toggle("Hide statusbar", isOn: $hideStatus)
+                }
+                .padding()
             }
-            .font(.largeTitle)
-            
-            Toggle("Hide statusbar", isOn: $hideStatus)
         }
-        .padding()
         .statusBar(hidden: hideStatus)
     }
 }
