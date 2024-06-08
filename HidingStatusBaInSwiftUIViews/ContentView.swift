620f123733a4d8204c8650f7a272c0f3a0b74a9d
ribragimov-mwb <ribragimov@malwarebytes.com>
2024-06-08 14:59:13 +0300

    Initial Commit

---
+++ HidingStatusBaInSwiftUIViews/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HidingStatusBaInSwiftUIViews
+//
+//  Created by Ramill Ibragimov on 6/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HidingStatusBaInSwiftUIViews/HidingStatusBaInSwiftUIViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HidingStatusBaInSwiftUIViewsApp.swift
+//  HidingStatusBaInSwiftUIViews
+//
+//  Created by Ramill Ibragimov on 6/8/24.
+//
+
+import SwiftUI
+
+@main
+struct HidingStatusBaInSwiftUIViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
